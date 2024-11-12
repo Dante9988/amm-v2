79ce2227e0d2a1fa7ad1a05dd7971103c22de63f
root <root@DESKTOP-905TRSD>
2024-11-12 12:03:22 -0800

    Adds addLiquidity and initial swap functions

--- contracts/AMM.sol
+++ contracts/AMM.sol
@@ -4,6 +4,93 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 import "./Token.sol";
 
+// [] Manage Pool
+// [] Manage Deposits
+// [] Manage Withdrawals
+// [] Facilitate Swaps
 contract AMM {
-    
+    Token public token1;
+    Token public token2;
+
+    uint public token1Balance;
+    uint public token2Balance;
+    uint public K;
+
+    uint public totalShares;
+    mapping(address => uint) public shares;
+    uint constant PRECISION = 10 ** 18;
+
+    constructor(Token _token1, Token _token2) {
+        token1 = _token1;
+        token2 = _token2;
+    }
+
+    function addLiquidity(uint _token1Amount, uint _token2Amount) external {
+        require(
+            token1.transferFrom(msg.sender, address(this), _token1Amount),
+            "Failed to transfer token1 from sender"
+        );
+        require(
+            token2.transferFrom(msg.sender, address(this), _token2Amount), 
+            "Failed to transfer token2 from sender"
+        );
+
+        // Issue shares
+        uint share;
+        // If no shares, issue 100% of the pool
+        if (totalShares == 0) {
+            share = 100 * PRECISION;
+        } else {
+            uint share1 = (totalShares * _token1Amount) / token1Balance; // get share of token1
+            uint share2 = (totalShares * _token2Amount) / token2Balance; // get share of token2
+            require((share1 / 10**3) == (share2 / 10**3), "Shares must be equal");
+            share = share1;
+        }
+
+        // Manage Pool
+        token1Balance += _token1Amount;
+        token2Balance += _token2Amount;
+        K = token1Balance * token2Balance; // x * y = k (formula for constant product AMM)
+
+        // Update shares
+        totalShares += share;
+        shares[msg.sender] += share;
+    }
+
+    // Determine how many token2 tokens must be deposited to get a certain amount of token1 tokens
+    function calculateToken2Deposit(uint _token1Amount) public view returns (uint token2Amount) {
+        token2Amount = (token2Balance * _token1Amount) / token1Balance;
+    }
+
+    // Determine how many token1 tokens must be deposited to get a certain amount of token2 tokens
+    function calculateToken1Deposit(uint _token2Amount) public view returns (uint token1Amount) {
+        token1Amount = (token1Balance * _token2Amount) / token2Balance;
+    }
+
+    function calculateToken1Swap(uint _token1Amount) public view returns (uint token2Amount) {
+        // Calculate token 2 amount
+        uint token1After = token1Balance + _token1Amount;
+        uint token2After = K / token1After;
+        token2Amount = token2Balance - token2After;
+
+        if (token2Amount == token2Balance) {
+            token2Amount--;
+        }
+
+        require(token2Amount < token2Balance, "Swap cannot exceed token2 balance");
+    }
+
+    function swapExactInput(uint _token1Amount) external returns (uint token2Amount) {
+        // Calculate token 2 amount
+        token2Amount = calculateToken1Swap(_token1Amount);
+
+        // Do swap
+
+        // Emit event
+
+    }
+
+    function swapExactOutput(uint _token2Amount) external returns (uint token1Amount) {
+
+    }
 }
--- test/AMM.js
+++ test/AMM.js
@@ -5,16 +5,111 @@ const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
+const formatEther = (n) => {
+  return ethers.utils.formatUnits(n, 'ether');
+}
+
 const ether = tokens
 const shares = ether
 
 describe('AMM', () => {
+  let accounts, 
+  deployer,
+  liquidityProvider
+  let token1, 
+  token2, 
+  amm;
 
+  let transaction, result;
+
+  beforeEach(async () => {
+    accounts = await ethers.getSigners();
+    deployer = accounts[0];
+    liquidityProvider = accounts[1];
+
+    const Token = await ethers.getContractFactory('Token');
+    token1 = await Token.deploy('Dapp University', 'DAPP', '1000000');
+    token2 = await Token.deploy('USD Token', 'USD', '1000000');
+
+    transaction = await token1.connect(deployer).transfer(liquidityProvider.address, tokens(100000));
+    result = await transaction.wait();
+
+    transaction = await token2.connect(deployer).transfer(liquidityProvider.address, tokens(100000));
+    result = await transaction.wait();
+
+    const AMM = await ethers.getContractFactory('AMM');
+    amm = await AMM.deploy(token1.address, token2.address);
+
+    
+  
+  });
 
   describe('Deployment', () => {
 
-    it('', async () => {
-      
+    it('has an address', async () => {
+      expect(amm.address).to.not.equal(0x0);
+    })
+
+    it('tracks token1 address', async () => {
+      expect(await amm.token1()).to.equal(token1.address);
+    })
+
+    it('tracks token2 address', async () => {
+      expect(await amm.token2()).to.equal(token2.address);
+    })
+
+  })
+  
+  describe('Swapping tokens', () => {
+
+    it('facilitates swaps', async () => {
+      let amount = tokens(100000);
+      // Approve tokens
+      transaction = await token1.connect(deployer).approve(amm.address, amount);
+      result = await transaction.wait();
+
+      transaction = await token2.connect(deployer).approve(amm.address, amount);
+      result = await transaction.wait();
+
+      // Deployer adds liquidity
+      transaction = await amm.connect(deployer).addLiquidity(amount, amount);
+      result = await transaction.wait();
+
+      // Check AMM received tokens
+      expect(await token1.balanceOf(amm.address)).to.equal(amount);
+      expect(await token2.balanceOf(amm.address)).to.equal(amount);
+
+      // Check token balances
+      expect(await amm.token1Balance()).to.equal(amount);
+      expect(await amm.token2Balance()).to.equal(amount);
+      // Check K
+      expect(await amm.K()).to.equal(amount.mul(amount));
+
+      // Check shares
+      expect(await amm.shares(deployer.address)).to.eq(tokens(100));
+      expect(await amm.totalShares()).to.eq(tokens(100));
+
+      // LP Adds more liquidity
+      amount = tokens(50000);
+      transaction = await token1.connect(liquidityProvider).approve(amm.address, amount);
+      result = await transaction.wait();
+
+      transaction = await token2.connect(liquidityProvider).approve(amm.address, amount);
+      result = await transaction.wait();
+
+      // Calculate token2 deposit
+      const token2Deposit = await amm.calculateToken2Deposit(amount);
+      transaction = await amm.connect(liquidityProvider).addLiquidity(amount, token2Deposit);
+      result = await transaction.wait();
+
+      // Check shares
+      expect(await amm.shares(liquidityProvider.address)).to.eq(tokens(50));
+
+      // deployer should have 100 shares
+      expect(await amm.shares(deployer.address)).to.eq(tokens(100));
+
+      // pool should have 150 shares
+      expect(await amm.totalShares()).to.eq(tokens(150));
     })
 
   })
